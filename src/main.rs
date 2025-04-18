34ac01590feb1a3bc369e0a1c8f4dab61a6c59db
Techtix0 <techtix0@proton.me>
2025-04-18 20:40:29 +0200

    Add: button layout

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,68 @@ use egui::{CentralPanel, RichText, TopBottomPanel};
 
 #[derive(Default)]
 struct TestApp {
-    value: f64,
+    value: String,
 }
 
 impl TestApp {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self::default()
+        Self {
+            value: String::new(),
+        }
+    }
+
+    fn render_buttons(&mut self, ui: &mut egui::Ui) {
+        ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
+            if ui.add(egui::Button::new("7").min_size(egui::Vec2::new(100.0, 100.0))).clicked() {
+                self.value += "7";
+            }
+
+            if ui.add(egui::Button::new("8").min_size(egui::Vec2::new(100.0, 100.0))).clicked() {
+                self.value += "8"
+            }
+
+            if ui.add(egui::Button::new("9").min_size(egui::Vec2::new(100.0, 100.0))).clicked() {
+                self.value += "9";
+            }
+
+        });
+
+        ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
+            if ui.add(egui::Button::new("4").min_size(egui::Vec2::new(100.0, 100.0))).clicked() {
+                self.value += "4";
+            }
+
+            if ui.add(egui::Button::new("5").min_size(egui::Vec2::new(100.0, 100.0))).clicked() {
+                self.value += "5"
+            }
+
+            if ui.add(egui::Button::new("6").min_size(egui::Vec2::new(100.0, 100.0))).clicked() {
+                self.value += "6";
+            }
+
+        });
+
+
+        ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
+            if ui.add(egui::Button::new("1").min_size(egui::Vec2::new(100.0, 100.0))).clicked() {
+                self.value += "1";
+            }
+
+            if ui.add(egui::Button::new("2").min_size(egui::Vec2::new(100.0, 100.0))).clicked() {
+                self.value += "2";
+            }
+
+            if ui.add(egui::Button::new("3").min_size(egui::Vec2::new(100.0, 100.0))).clicked() {
+                self.value += "3";
+            }
+        });
+
+
+        ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
+            if ui.add(egui::Button::new("0").min_size(egui::Vec2::new(100.0, 100.0))).clicked() {
+                self.value += "0";
+            }
+        });
     }
 }
 
@@ -21,10 +77,8 @@ impl eframe::App for TestApp {
             )
         });
 
-        CentralPanel::default().show(ctx, |ui| {
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
+        CentralPanel::default().show(ctx, |ui: &mut egui::Ui| {
+            self.render_buttons(ui);
         });
     }
 }
@@ -34,6 +88,6 @@ fn main() -> eframe::Result {
     run_native(
         "Calculator",
         native_options,
-        Box::new(|cc| Ok(Box::new(TestApp::new(cc))))
+        Box::new(|cc| Ok(Box::new(TestApp::new(cc)))),
     )
 }
